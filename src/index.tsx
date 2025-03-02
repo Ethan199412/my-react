edf91db07bceb48fcd55b1e0b68f245f19de8e6d
lijiahao1213ok@163.com <lijiahao1213ok@163.com>
2025-03-02 17:45:01 +0800

    tsconfig modify

--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,4 @@ import React from "./react/index.ts";
 import Todos from "./component/todos.tsx"
 import {TestDiff, TestDiff2} from './component/test-diff.tsx'
 
-React.render(<TestDiff />, document.getElementById("root"));
+React.render(<TestDiff />, document.getElementById("root")!);
